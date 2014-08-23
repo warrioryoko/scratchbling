b9f0149779d19a785a8bbe05f18d8749b7c06e05
warrioryoko <miangpellegri@gmail.com>
2014-08-22 19:32:58 -0700

    Updated security

--- sb/urls.py
+++ sb/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
 from scratchers.resources import ScratcherResource
+from django.contrib import admin
+
+admin.autodiscover()
 
 v1_api = Api(api_name='v1')
 v1_api.register(ScratcherResource())
@@ -10,5 +13,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'sb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+	url(r'^admin/', include(admin.site.urls)),
     (r'^api/', include(v1_api.urls)),
 )
