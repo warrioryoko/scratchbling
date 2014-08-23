45cb7371ae05dc76def024b6d06fc6102f65f3ec
warrioryoko <miangpellegri@gmail.com>
2014-08-22 18:36:41 -0700

    Updated item_cost

--- scratchers/models.py
+++ scratchers/models.py
@@ -4,4 +4,4 @@ class Scratcher(models.Model):
 
 	item_name = models.CharField(max_length=255)
 	item_description = models.TextField()
-	item_cost = models.SmallIntegerField()
+	item_cost = models.DecimalField(decimal_places=2, max_digits=10)
