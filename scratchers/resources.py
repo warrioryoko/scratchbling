a68fa2c31f60a2c39471de950ea43916f5d00dc2
warrioryoko <miangpellegri@gmail.com>
2014-08-22 19:14:40 -0700

    Updated resources and models

--- scratchers/resources.py
+++ scratchers/resources.py
@@ -2,11 +2,8 @@ from tastypie.resources import ModelResource
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from models import Scratcher
-from tastypie import fields
 
 class ScratcherResource(ModelResource):
-    item_cost = fields.DecimalField(attribute = 'item_cost')
-    
     class Meta:
     	
 		queryset = Scratcher.objects.filter()
