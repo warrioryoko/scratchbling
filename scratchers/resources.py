eaaf16c5fa6096b95f077c36f6f1eb41b5ddbb8c
warrioryoko <miangpellegri@gmail.com>
2014-08-22 19:53:53 -0700

    Added security

--- scratchers/resources.py
+++ scratchers/resources.py
@@ -1,12 +1,12 @@
 from tastypie.resources import ModelResource
-from tastypie.authentication import Authentication
-from tastypie.authorization import Authorization
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import DjangoAuthorization
 from models import Scratcher
 
 class ScratcherResource(ModelResource):
     class Meta:
     	
 		queryset = Scratcher.objects.filter()
-		#authentication = Authentication()
-		authorization = Authorization()
+		authentication = ApiKeyAuthentication()
+		authorization = DjangoAuthorization()
 
